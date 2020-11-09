8041cf15d3d352462f3c50b9cba067bcc89e454d
Beatso <39217836+Beatso1@users.noreply.github.com>
2020-11-09 19:36:52 +0000

    update server message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 server.all('/', (req, res)=>{
-    res.send('Thanks for waking up Bjorn!')
+    res.send('Thanks for waking up Chubot!')
 })
 function keepAlive(){
     server.listen(3000, ()=>{console.log("server running")});
